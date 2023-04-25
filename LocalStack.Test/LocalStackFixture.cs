ed1bd583e0c5db286b5710ead13e7faa70051bcc
Oleh Halay <o.galai@temabit.com>
2023-04-25 19:04:52 +0300

    Init repo with test

---
+++ LocalStack.Test/LocalStackFixture.cs
@@ -0,0 +1,29 @@
+﻿using DotNet.Testcontainers.Builders;
+using Testcontainers.LocalStack;
+
+namespace LocalStack.Test;
+
+public class LocalStackFixture : IAsyncLifetime
+{
+    public const string BUCKETNAME = "test-bucket";
+
+    public readonly LocalStackContainer Container = new LocalStackBuilder()
+        .WithImage("localstack/localstack:latest")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted(new[]
+        {
+            "awslocal", "s3api", "create-bucket", "--bucket", BUCKETNAME,
+        }))
+        .Build();
+
+    static LocalStackFixture()
+    {
+        Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", "my-test-key");
+        Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "my-test-secret-key");
+    }
+
+    public Task DisposeAsync()
+        => Container.StopAsync();
+
+    public Task InitializeAsync()
+        => Container.StartAsync();
+}
